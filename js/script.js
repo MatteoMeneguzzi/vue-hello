4037fc40998744a1d365447d3ca4d4b78362b6ad
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-04-20 15:33:27 +0200

    complete

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+	el: "#app",
+	data: {
+		message: "Hello Vuewer!",
+		image:
+			"https://preview.redd.it/19fq7c002w021.png?width=960&crop=smart&auto=webp&s=6f07026d1381361b7a1716442cfc86c85d567047",
+	},
+});
